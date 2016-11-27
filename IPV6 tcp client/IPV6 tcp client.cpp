6202e668c73393eac24e961bb4da7a55f96de9e1
Zoltan Szanto <mrbig00@gmail.com>
2016-11-27 13:01:42 +0200

    project init

--- IPV6 tcp client/IPV6 tcp client.cpp
+++ IPV6 tcp client/IPV6 tcp client.cpp
@@ -3,9 +3,130 @@
 
 #include "stdafx.h"
 
+#define WIN32_LEAN_AND_MEAN
 
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "8000"
+#define DEFAULT_ADDRESS "::1"
 int main()
 {
-    return 0;
+	WSADATA wsaData;
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+	char *sendbuf = "this is a test";
+	char recvbuf[DEFAULT_BUFLEN];
+	int iResult;
+	int recvbuflen = DEFAULT_BUFLEN;
+
+
+	// Winsock initializálás
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed with error: %d\n", iResult);
+		system("pause");
+		return 1;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	// Ip cim es port beallitas
+	iResult = getaddrinfo(DEFAULT_ADDRESS, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		system("pause");
+		return 1;
+	}
+
+	// Kapcsolódás egy címhez
+	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+
+		// Socket létrehozása
+		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+		if (ConnectSocket == INVALID_SOCKET) {
+			printf("socket failed with error: %ld\n", WSAGetLastError());
+			WSACleanup();
+			system("pause");
+			return 1;
+		}
+
+		// Kapcsolódás a serverhez
+		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+		if (iResult == SOCKET_ERROR) {
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+			continue;
+		}
+		break;
+	}
+
+	freeaddrinfo(result);
+
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server!\n");
+		WSACleanup();
+		system("pause");
+		return 1;
+	}
+
+	// üzenet küldése
+	iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+	if (iResult == SOCKET_ERROR) {
+		printf("send failed with error: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		system("pause");
+		return 1;
+	}
+
+	printf("Bytes Sent: %ld\n", iResult);
+
+
+	// Üzenet fogadása
+	do {
+
+		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+		if (iResult > 0) {
+			printf("Message received: %s\n", recvbuf);
+		}
+		else if (iResult == 0) {
+			printf("Connection closed\n");
+		}
+		else {
+			printf("recv failed with error: %d\n", WSAGetLastError());
+		}
+
+	} while (iResult > 0);
+
+
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		system("pause");
+		return 1;
+	}
+	closesocket(ConnectSocket);
+	WSACleanup();
+	system("pause");
+	return 0;
 }
 
